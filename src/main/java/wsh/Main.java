fd3104cb4912c54d9c56304e2a27c84ceec6c5f5
VerseVent <60715161+VerseVent@users.noreply.github.com>
2020-12-01 09:47:09 +0100

    Delete Main.java

--- src/main/java/wsh/Main.java
+++
@@ -1,9 +0,0 @@
-package wsh;
-
-public class Main {
-    public static void main(String[] args) {
-        String pussy1 = "pussy";
-        String pussy2="pussy";
-        System.out.println(pussy1.matches(pussy2));
-    }
-}
